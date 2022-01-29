30fb4b8dfeeb7b3d1a5565058446db7661c239f8
MaverickSevmont (Erick Sevilla) <sevmont@gmail.com>
2022-01-29 11:50:25 -0600

    Update PHPCode.php

--- 01 - Configure PHP and IIS on Windows Server/PHPCode.php
+++ 01 - Configure PHP and IIS on Windows Server/PHPCode.php
@@ -1,3 +1,9 @@
 <?php
 
+; Uncomment (remove ;) and set the following in php.ini
+; fastcgi.impersonate = 1
+; extension_dir = "ext\"
+; cgi.force_redirect = 0
+; fastcgi.logging = 0
+
 echo "hello " . get_current_user();
