a97774843d83af09bf3bbad7d137980da0dea496
MaverickSevmont (Erick Sevilla) <sevmont@gmail.com>
2022-01-29 11:43:05 -0600

    Create PowerShellCode.ps1

---
+++ 01 - Configure PHP and IIS on Windows Server/PowerShellCode.ps1
@@ -0,0 +1,6 @@
+# Playlist : https://www.youtube.com/playlist?list=PL60ejEuI_nxuWG17kyScAnpU-EQMOaN9x
+# Video    : https://www.youtube.com/watch?v=b_Jm-hA0RAk&list=PL60ejEuI_nxuWG17kyScAnpU-EQMOaN9x&index=2
+# Title    : 01 - Configure PHP and IIS on Windows Server
+
+# Install needed Windows features
+Install-WindowsFeature -Name Web-Server, Web-CGI, Web-Windows-Auth, RSAT-AD-Tools, RSAT-AD-PowerShell -IncludeManagementTools
