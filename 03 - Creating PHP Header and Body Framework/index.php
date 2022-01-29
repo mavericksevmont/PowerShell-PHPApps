b98ca265e33b5ca2ad614fbbe1e55faa1624781a
MaverickSevmont (Erick Sevilla) <sevmont@gmail.com>
2022-01-29 13:53:10 -0600

    Update index.php

--- 03 - Creating PHP Header and Body Framework/index.php
+++ 03 - Creating PHP Header and Body Framework/index.php
@@ -1,4 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Webtool Automation</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/styleCustom.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+</head>
+<body>
+    <header id="masthead" class="site-header" role="banner">
+        <div class="clearfix container">
+            <div> class="site branding">
+                <a href="/" title="the Sysadmin Channel"><img src="/img/logo.png" alt="the Sysadmin Channel"></a>
+            </div>
+        </div>
+
+        <nav id="main-navigation" class="main-navigation" role="navigation">
+            <a href="#main-navigation" class="nav-open">Menu</a>
+            <a href="#" class="nav-close">Close</a>
+            
+            <div class="clearfix sf-menu">
+                <ul class="menu-my-menu" class="menu sf-js-enabled">
+                    <li id="menu-item-10" class="menu-item"><a href="/">Home</a> </li>
+                    <li id="menu-item-12" class="menu-item"><a href="/newhire/">New Hire</a> </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
+
 <?php
 
 phpinfo();
 
+?>
